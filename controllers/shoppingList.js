5f6b50b7f215f4cee161ff0ecda6e4e1f16d5997
Niladri <niladri.sinha89@gmail.com>
2021-05-26 23:07:10 -0400

    adds create and update functions

--- controllers/shoppingList.js
+++ controllers/shoppingList.js
@@ -1,12 +1,27 @@
-const Skill = require('../models/shoppingList');
+const ShoppingList = require('../models/shoppingList');
 
 module.exports = {
     index,
-  
+    create,
+    update,
+
 };
 
 function index(req, res) {
-    Skill.find(function(){
-        res.status(200)
+    ShoppingList.find({}, function (err, shoppingList) {
+        res.status(200).json(shoppingList);
     });
 }
+
+function create(req, res) {
+    ShoppingList.create(req.body, function (err, shoppingList) {
+        res.status(201).json(shoppingList);
+    });
+}
+
+function update(req, res) {
+    ShoppingList.findByIdAndUpdate(req.params.id, req.body, function (err, shoppingList) {
+        index(req, res);
+    });
+}
+
