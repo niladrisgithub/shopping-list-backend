739906f0a06ffd4111792b204290b01a62141702
Niladri <niladri.sinha89@gmail.com>
2021-06-01 22:59:53 -0400

    feat: complete setting routes

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const shoppingListCtrl = require('../controllers/shoppingList')
 
-router.get('/', shoppingListCtrl.index)
+router.get('/', shoppingListCtrl.index);
+router.post('/', shoppingListCtrl.create);
+router.put('/:id', shoppingListCtrl.update);
+router.delete('/:id', shoppingListCtrl.delete);
 
 
 module.exports = router;
