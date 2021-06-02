b67d030cc1c6d8c77d4a23994895d046b88a269b
Niladri <niladri.sinha89@gmail.com>
2021-06-01 23:16:02 -0400

    wip: adding firebase

--- routes/shoppingList.js
+++ routes/shoppingList.js
@@ -2,10 +2,14 @@ const express = require('express');
 const router = express.Router();
 const shoppingListCtrl = require('../controllers/shoppingList')
 
-router.get('/', shoppingListCtrl.index);
+router.get('/', isAuthenticated, shoppingListCtrl.index);
 router.post('/', shoppingListCtrl.create);
-router.put('/:id', shoppingListCtrl.update);
-router.delete('/:id', shoppingListCtrl.delete);
+router.put('/:id', isAuthenticated, shoppingListCtrl.update);
+router.delete('/:id', isAuthenticated, shoppingListCtrl.delete);
 
+function isAuthenticated(req, res, next) {
+    if(!req.query.uid) return res.status(401).json({error: 'user must be logged in'});
+    next();
+}
 
 module.exports = router;
